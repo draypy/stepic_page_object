44cc21472d15f89fc7d863ea40102e4ff60a4cf6
Artem Kasperovich <kaspiarovich.artsemi@gmail.com>
2022-02-06 17:44:17 +0300

    feat: implemented test case for login page

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_SUBMIT = (By.NAME, "login_submit")
+    REGISTER_SUBMIT = (By.NAME, 'registration_submit')
